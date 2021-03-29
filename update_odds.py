9215c83cba571bf77f1c9b01e44addb57c848b34
manish1772 <vs.manish1997@gmail.com>
2021-03-29 12:56:46 +0100

    initial commit

---
+++ update_odds.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# DISABLED LINE
+# import pymongo
+# DISABLED LINE
+# import json
+# DISABLED LINE
+# import requests
+# DISABLED LINE
+# import os
+# DISABLED LINE
+# from dotenv import load_dotenv
+
+
+# In[3]:
+
+
+# DISABLED LINE
+# load_dotenv()
+
+
+# In[4]:
+
+
+# Establish a connection with mongoDB
+
+
+# In[20]:
+
+
+
+
+# In[21]:
+
+
+# DISABLED LINE
+# def Uk_odds():
+
+# DISABLED LINE
+#     if "Uk_Odds" not in dataBase.list_collection_names():
+# DISABLED LINE
+#     else:
+# DISABLED LINE
+#             'api_key': API_KEY,
+# DISABLED LINE
+#             'sport': sport_key,
+# DISABLED LINE
+#             'region': 'uk', 
+# DISABLED LINE
+#             'mkt': 'h2h', 
+# DISABLED LINE
+#             'dateFormat': 'iso'})
+
+# DISABLED LINE
+#     return odds_response
+
+
+
+# In[22]:
+
+
+
+
+# In[23]:
+
+
+
+# DISABLED LINE
+# Odds_uk.delete_many({})
+# DISABLED LINE
+# Odds_uk.insert_many(in_play_data['data'])
+
+
+# In[126]:
+
+
+# DISABLED LINE
+# import pandas as pd
+
+
+# In[25]:
+
+
+
+
+# In[125]:
+
+
+# DISABLED LINE
+# data.head()
+
+
+# In[26]:
+
+
+
+# DISABLED LINE
+# for i in range(len(data['sites'])):
+# DISABLED LINE
+#     for j in range(len(data['sites'][i])):
+# DISABLED LINE
+#         odds.append(data['sites'][i][j]['odds'])
+
+
+
+
+# In[27]:
+
+
+# DISABLED LINE
+# for i in range(len(data['sites'])):
+# DISABLED LINE
+#     for j in range(len(data['sites'][i])):
+# DISABLED LINE
+#         sites.append(data['sites'][i][j]['site_key'])
+
+
+
+
+# In[120]:
+
+
+# DISABLED LINE
+# for i in range(len(data)):
+# DISABLED LINE
+#     m.append(l)
+
+# DISABLED LINE
+# for j in range(len(m)):
+# DISABLED LINE
+#     for i in m[j]:
+# DISABLED LINE
+#         teams.append(i)                   
+
+
+
+# In[116]:
+
+
+# DISABLED LINE
+# for i in range(len(data)):
+# DISABLED LINE
+#     m.append(l)
+
+# DISABLED LINE
+# for j in range(len(m)):
+# DISABLED LINE
+#     for i in m[j]:
+# DISABLED LINE
+#         leagues.append(i)                   
+
+
+
+# In[122]:
+
+
+
+
+# In[124]:
+
+
+# DISABLED LINE
+# Final_data.head()
+
+
+# In[ ]:
+
+
+
+
+*/10 * * * * update_odds.ipynb.
+*/10 * * * * update_odds.py
